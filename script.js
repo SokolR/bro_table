ec0d4e11fc42c562ff0e63a174325bc24d1bf055
SokolR <sokolruslan92@gmail.com>
2018-08-28 01:41:56 +0300

    add visible and hide buttons

--- script.js
+++ script.js
@@ -7,16 +7,17 @@ var element = document.querySelector(ELEMENT_FOR_INSERT);
 //Создание кнопок
 
 function createButtons() {
-  var btnWrapper = document.createElement('div');
-  btnWrapper.classList.add('wrapper-buttons');
-  btnWrapper.classList.add('btn');
+  //var btnWrapper = document.createElement('div');
+  //btnWrapper.classList.add('wrapper-buttons');
+  //btnWrapper.classList.add('btn');
 
   var btnRight = document.createElement('button');
   btnRight.classList.add('btn');
   btnRight.classList.add('btn__right');
   btnRight.classList.add('btn__add');
   btnRight.innerText = '+';
-  btnWrapper.appendChild(btnRight);
+
+  element.appendChild(btnRight);
 
   var btnBottom = document.createElement('button');
   btnBottom.classList.add('btn');
@@ -24,24 +25,26 @@ function createButtons() {
   btnBottom.classList.add('btn__add');
   btnBottom.innerText = '+';
 
-  btnWrapper.appendChild(btnBottom);
+  element.appendChild(btnBottom);
 
   var btnTop = document.createElement('button');
   btnTop.classList.add('btn');
   btnTop.classList.add('btn__top');
   btnTop.classList.add('btn__delete'); 
+  btnTop.classList.add('disabled');
   btnTop.innerText = '-';
 
-  btnWrapper.appendChild(btnTop);
+  element.appendChild(btnTop);
 
   var btnLeft = document.createElement('button');
   btnLeft.classList.add('btn');
   btnLeft.classList.add('btn__left');
   btnLeft.classList.add('btn__delete');  
+  btnLeft.classList.add('disabled');
   btnLeft.innerText = '-';
-  btnWrapper.appendChild(btnLeft);
+  element.appendChild(btnLeft);  
      
-  element.appendChild(btnWrapper);
+  //element.appendChild(btnWrapper);
 }
 createButtons();
 
@@ -75,9 +78,6 @@ var btnBot = document.querySelector('.btn__bottom');
 var btnTop = document.querySelector('.btn__top');
 var btnLeft = document.querySelector('.btn__left');
 
-// btnTop.classList.add('disabled');
-//btnLeft.classList.add('disabled');
-
 var table = document.querySelector('table');
 
 //Функция добавление строк
@@ -93,15 +93,19 @@ function addRow() {
     }
     table.appendChild(rows);
   }
+  oneRow();
+  moveRowsAddBtn();
 }
 
 //Функция удаление строк
 
 function delRow() {
   for (var i = 0; i < 1; i++) {
-    oneRow();
+    
     table.deleteRow(-1);
   }
+  oneRow();
+  moveRowsAddBtn();
 }
 
 //Функция добавление колонок
@@ -113,6 +117,8 @@ function addColumn() {
     var cells = document.createElement('td');
     tableColumns[i].appendChild(cells);
   }
+  oneColumn();
+  moveCollumnsAddBtn();
 }
 
 //Функция удаление колонок
@@ -121,33 +127,108 @@ function delColumn() {
 var tableColumns = document.querySelectorAll('tr');
 
   for (i = 0; i < tableColumns.length; i++) {
-    oneColumn();
+    
     tableColumns[i].deleteCell(-1);
   }
+  moveCollumnsAddBtn();
+  oneColumn();
+  
 }
 
 //Проверка количества строк
 
 function oneRow() {
-  var tableRows = document.querySelector('tr');
- 
-  if (tableRows.length <= 1) {
+  if (table.rows.length < 2) {
     btnLeft.style = 'display: none';
+  }  
+  else {
+    btnLeft.removeAttribute("style")
   }
 }
 
 //Проверка количества столбцов
 
 function oneColumn() {
-  var tableColumns = document.querySelector('tr'); 
+  var oneColumn = document.querySelector('tr'); 
 
-  if (tableColumns.cells.length <= 1) {
+  if (oneColumn.cells.length < 2) {
      btnTop.style = 'display: none';
-  }
+  } 
+  else {
+    btnTop.removeAttribute("style")
+  } 
 }
 
+//Отобразить кнопки удаления
+
+function btnsDelVisible() {
+  leftBtnVisible();
+  topBtnVisible();
+}
+
+//Скрыть кнопки удаления
+
+function btnsDelHide() {
+  leftBtnHide();
+  topBtnHide();
+}
+
+function leftBtnVisible() {
+  btnLeft.classList.remove('disabled');
+}
+
+function leftBtnHide() {
+  btnLeft.classList.add('disabled');
+}
+
+function topBtnVisible() {
+  btnTop.classList.remove('disabled');
+}
+
+function topBtnHide() {
+  btnTop.classList.add('disabled');
+}
+
+//Смещение кнопки при добавлении строки
+
+function moveRowsAddBtn() {
+  var coordinate = table.getBoundingClientRect();
+  
+  btnBot.style.top = `${coordinate.height}px`;
+}
+
+//Смещение кнопки при добавлении колонки
+
+function moveCollumnsAddBtn() {
+  var coordinate = table.getBoundingClientRect();
+  
+  btnRight.style.left = `${coordinate.width}px`;
+}
+
+
+table.addEventListener('mouseover', btnsDelVisible);     //Появление кнопок удаления при наведении курсора на таблицу
+
+table.addEventListener('mouseout', btnsDelHide);     //Пропадание кнопок удаления при отведении курсора с таблици
+
 btnBot.addEventListener('click', addRow);     //Добавление строки по клику на кнопку
+
 btnRight.addEventListener('click', addColumn);      //Добавление колонки по клику на кнопку
+
 btnLeft.addEventListener('click', delRow);     //Удаление строки по клику на кнопку
+
+btnLeft.addEventListener('mouseover', leftBtnVisible);     //Появление кнопки при наведении курсора на кнопку
+
+btnLeft.addEventListener('mouseout', leftBtnHide);     //Пропадание кнопки при отведении курсора с конпки
+
+btnLeft.addEventListener('click', leftBtnHide);     //Пропадание кнопки при клике по ней
+
 btnTop.addEventListener('click', delColumn);     //Удаление колонки по клику на кнопку
+
+btnTop.addEventListener('click', delColumn);     //Удаление колонки по клику на кнопку
+
+btnTop.addEventListener('mouseover', topBtnVisible);     //Появление кнопки при наведении курсора на кнопку
+
+btnTop.addEventListener('mouseout', topBtnHide);     //Пропадание кнопки при отведении курсора с конпки
+
+btnTop.addEventListener('click', topBtnHide);     //Пропадание кнопки при клике по ней
 
